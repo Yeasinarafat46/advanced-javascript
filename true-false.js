200937709afe06d9d9d649703c086e045b47e355
yeasinarafat100 <md.1999arafat@gmail.com>
2020-08-25 01:20:55 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+
+// Falsy
+// 0
+// ""
+//null
+// NaN
+// undefined
+// "0" " " []
+
+
+
+
+let name = 12;
+
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
